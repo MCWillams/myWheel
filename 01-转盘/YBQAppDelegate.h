48dc2ed4e9f9b8826a8ea4c83ee5baff866a5d70
MCWillams <yanbiqing@yanbiqingdeMac.local>
2015-06-06 07:30:08 -0700

    Initial Commit

---
+++ 01-转盘/YBQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  YBQAppDelegate.h
+//  01-转盘
+//
+//  Created by 严必庆 on 15-6-6.
+//  Copyright (c) 2015年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YBQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
